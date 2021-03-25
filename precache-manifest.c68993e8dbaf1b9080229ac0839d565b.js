51ccd6cde4cc8786ab52e176b7d9b030c00c0190
thaomike <51148093+thaomike@users.noreply.github.com>
2021-03-25 16:00:25 +0700

    Updates

---
+++ precache-manifest.c68993e8dbaf1b9080229ac0839d565b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed45d85e00d1a55e8b546bfe4139185a",
+    "url": "/ldp/index.html"
+  },
+  {
+    "revision": "6f2901c6fb766c765ee5",
+    "url": "/ldp/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "fa7fb98010c1b05db349",
+    "url": "/ldp/static/css/main.8659d79b.chunk.css"
+  },
+  {
+    "revision": "6f2901c6fb766c765ee5",
+    "url": "/ldp/static/js/2.2b70f7d5.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/ldp/static/js/2.2b70f7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62389015a7b56544a0b",
+    "url": "/ldp/static/js/3.5a199a55.chunk.js"
+  },
+  {
+    "revision": "fa7fb98010c1b05db349",
+    "url": "/ldp/static/js/main.db030737.chunk.js"
+  },
+  {
+    "revision": "0c5d15cf09103d573175",
+    "url": "/ldp/static/js/runtime-main.bab75940.js"
+  },
+  {
+    "revision": "a5ae8609ffa92329c2f0745eb91e50f8",
+    "url": "/ldp/static/media/ProPTIT.a5ae8609.png"
+  },
+  {
+    "revision": "6274483708388f3a5edf5b47f72d25eb",
+    "url": "/ldp/static/media/Tournament.62744837.png"
+  },
+  {
+    "revision": "3649028165d399e1478d51d5ea00bea0",
+    "url": "/ldp/static/media/clb.36490281.JPG"
+  },
+  {
+    "revision": "29ac349103f1b0ce5cbf3b94329fdf2a",
+    "url": "/ldp/static/media/diamond.29ac3491.png"
+  },
+  {
+    "revision": "5586bb3e17262b3e2e9b71d19a44710d",
+    "url": "/ldp/static/media/game.5586bb3e.jpg"
+  },
+  {
+    "revision": "7a00e6d958a5e202c76b13780767eacf",
+    "url": "/ldp/static/media/gold.7a00e6d9.png"
+  },
+  {
+    "revision": "8144ca794a351e44be6cc114a3a1daad",
+    "url": "/ldp/static/media/huong.8144ca79.jpg"
+  },
+  {
+    "revision": "86fcc600d7b6f8ec4e1ad8c6622b2312",
+    "url": "/ldp/static/media/introD17.86fcc600.jpg"
+  },
+  {
+    "revision": "416d36a1f4af1d91c55eeaa670db1734",
+    "url": "/ldp/static/media/introD18.416d36a1.jpg"
+  },
+  {
+    "revision": "83c8bc537428b14dd0f6b964d8150863",
+    "url": "/ldp/static/media/linh.83c8bc53.jpg"
+  },
+  {
+    "revision": "7b37190770ee79e71f488d69ab2cfaec",
+    "url": "/ldp/static/media/silver.7b371907.png"
+  },
+  {
+    "revision": "3f889a5fa9753179af4a51a9c211b67a",
+    "url": "/ldp/static/media/sinhNhats2b.3f889a5f.jpg"
+  }
+]);
