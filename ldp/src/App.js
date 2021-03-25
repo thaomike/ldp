20d639409ec1e70ee8c6e437d5201fbb5bcb96b0
thaomike <51148093+thaomike@users.noreply.github.com>
2021-03-25 15:54:11 +0700

    pull all

---
+++ ldp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import AboutUs from './LadingPage/AboutUs'
+
+function App() {
+  return (
+    <div className="App">
+      <AboutUs/>
+      {/* <p>Hel</p> */}
+    </div>
+  );
+}
+
+export default App;
